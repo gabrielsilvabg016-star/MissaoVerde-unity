e43b2a8e049c1ba0370a1431204e53a93082e3c6
Gabriel Silveira <gabrielsilva.bg016@academico.ifsul.edu.br>
2025-11-24 11:12:38 -0300

    Versão 6 - ok
    [ADIÇÃO] botão de ajuda que apresenta uma tabela ao ser clicado e seu script.
    [ADIÇÃO] sprites utilizados para o botão de ajuda e para a tabela a ser mostrada.
    [AJUSTE] pareamento da cena ReciclagemFase02 concertado.

---
+++ Assets/Scripts/tabelaAuxiliar.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class tabelaAuxiliar : MonoBehaviour
+{
+    public GameObject imagemTabela;
+    public Button botao;
+    void Start() 
+    {
+        if (imagemTabela != null)
+        {
+            imagemTabela.SetActive(false); //seta a imagem como escondida
+        }
+        botao.onClick.AddListener(MostrarImagem);
+    }
+
+    public void MostrarImagem()
+    {
+        Debug.Log("botão funciona");
+        if(imagemTabela.activeSelf == false)
+        {
+            imagemTabela.SetActive(true); // esconde a imagem
+        }
+        else if (imagemTabela.activeSelf == true)
+        {
+            imagemTabela.SetActive(false);
+        }
+    }
+}
