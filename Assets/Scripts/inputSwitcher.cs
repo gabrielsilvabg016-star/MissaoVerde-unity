eeb62e6857e5e76ae2128a997db012eed446fda8
gabrielsilvabg016-star <gabrielsilva.bg016@academico.ifsul.edu.br>
2026-04-14 10:29:08 -0300

    Versão - 19.54
    Adicionado comentários para melhor explicar o script inputSwitcher e adicionado neste mesmo script uma forma de tocar um som ao navegar via setas, isso será transformado em uma função mais tarde para ser melhor mantido

--- Assets/Scripts/inputSwitcher.cs
+++ Assets/Scripts/inputSwitcher.cs
@@ -4,13 +4,20 @@ using UnityEngine.EventSystems;
 public class InputSwitcher : MonoBehaviour
 {
     public GameObject primeiroBotao;
-
     private bool Mouse = true;
+    private AudioSource audioSource;
+    public AudioClip SomMovimento;
+    private GameObject ultimoSelect;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        DetectMouse();
-        DetectTeclado();
+        DetectMouse();//mouse = hover
+        DetectTeclado();//teclado = seleção
     }
 
     void DetectMouse()
@@ -21,16 +28,17 @@ public class InputSwitcher : MonoBehaviour
             if(!Mouse)
             {
                 Mouse = true;
-                EventSystem.current.SetSelectedGameObject(null);
+                EventSystem.current.SetSelectedGameObject(null); //remove a seleção do menu
             }
         }
 
+        //clique do mouse
         if(Input.GetMouseButtonDown(0))
         {
             if(!Mouse)
             {
                 Mouse = true;
-                EventSystem.current.SetSelectedGameObject(null);
+                EventSystem.current.SetSelectedGameObject(null); //remove a seleção do menu
             }
         }
     }
@@ -39,7 +47,7 @@ public class InputSwitcher : MonoBehaviour
         //pressionamento de uma tecla
         if(Input.anyKeyDown)
         {
-            if(Input.GetMouseButtonDown(0))
+            if(Input.GetMouseButtonDown(0))//clique do mouse conta como anyKeyDown, isso evita conflito
                 return;
 
             if(Mouse)
@@ -48,11 +56,23 @@ public class InputSwitcher : MonoBehaviour
 
                 if(EventSystem.current.currentSelectedGameObject == null)
                 {
-                    EventSystem.current.SetSelectedGameObject(primeiroBotao);
+                    EventSystem.current.SetSelectedGameObject(primeiroBotao);//nenhum botão selecionado, seleciona a variavel primeiro botão
+                }
+                
+                GameObject current = EventSystem.current.currentSelectedGameObject;
+                
+                if(current != ultimoSelect)
+                {
+                    if(current != null)
+                    {
+                        audioSource.PlayOneShot(SomMovimento);
+                    }
+                    ultimoSelect = current;
                 }
             }
         }
 
+        //detecta teclas WASD e analogico de controle
         if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             if(Mouse)
@@ -63,6 +83,17 @@ public class InputSwitcher : MonoBehaviour
                 {
                     EventSystem.current.SetSelectedGameObject(primeiroBotao);
                 }
+            }
+
+            GameObject current = EventSystem.current.currentSelectedGameObject;
+                
+            if(current != ultimoSelect)
+            {
+                if(current != null)
+                {
+                    audioSource.PlayOneShot(SomMovimento);
+                }
+                ultimoSelect = current;
             }
         }
     }
