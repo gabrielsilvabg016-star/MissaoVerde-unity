d7e4d005e3c69abde71a3ed052f5c34232b82cb1
gabrielsilvabg016-star <gabrielsilva.bg016@academico.ifsul.edu.br>
2026-06-12 14:54:59 -0300

    Update ObjetosSom.cs

--- Assets/Scripts/ObjetosSom.cs
+++ Assets/Scripts/ObjetosSom.cs
@@ -8,7 +8,7 @@ public class ObjetosSom : MonoBehaviour //script que captura ambos objetos e som
 
     void Start()
     {
-        if(InputSwitcher.NavSom)
+        if(inputSwitcher.NavSom)
         {
             ConfigurarAudio();
         }
