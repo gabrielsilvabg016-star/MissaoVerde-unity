fbe8e424c2ffeb8c0462fae035fcdbf050694865
Gabriel Silveira <gabrielsilva.bg016@academico.ifsul.edu.br>
2026-07-31 10:07:46 -0300

    19.66 - OK
    - Erro do sistema de pareamento nas versoões de Web corrigido.
    
    Solução: no editor da unity os objetos eram criados primeiro que o codigo do controllerSetas, no web e o contrario, controllerSetas e criado e rodado primeiro que os objetos. Para resolver isso foi criada uma função no controllerSetas que e chamada no update e para impedir que essa função seja chamada diversas vezes foi utilizado uma variavel booleana como trava em um IF aninhado.

--- Assets/Scripts/ControllerSetas.cs
+++ Assets/Scripts/ControllerSetas.cs
@@ -16,6 +16,7 @@ public class ControllerSetas : MonoBehaviour
     public AudioClip somAcerto;
     public AudioClip somErro;
     private bool travaBotao = false;
+    private bool travaCriacaoObjetos = false;
     private int numObjetos;
     private int numSombras;
     private int quantItems;
@@ -26,37 +27,23 @@ public class ControllerSetas : MonoBehaviour
     }
     void Start()
     {
-        numObjetos = painelObjetos.transform.childCount;
-        numSombras = painelLixeiras.transform.childCount;
-
-        for(int x = 0; x<numObjetos; x++)//captura os filhos do painel, pega o elemento button e adiciona a função ClicarLixo
-        {
-            GameObject filho = painelObjetos.transform.GetChild(x).gameObject;
-            Button btn = filho.GetComponent<Button>();
-
-            btn.onClick.AddListener(() => {
-            Debug.Log(filho == null);
-            ClicarLixo(filho);
-            });
-        }
-
-        for(int x = 0; x<numSombras; x++)//captura os filhos do painel, pega o elemento button e adiciona a função ClicarLixeira
-        {
-            GameObject filho = painelLixeiras.transform.GetChild(x).gameObject;
-            Button btn = filho.GetComponent<Button>();
-
-            btn.onClick.AddListener(() => {
-            Debug.Log(filho == null);
-            ClicarLixeira(filho);
-            });
-        }
-
-        audioSource = GetComponent<AudioSource>();//saida de audio geral para o sistema de pareamento
+        Debug.Log("O controllerSetas carregou");
+        Debug.Log($"quantiade de objetos no inicio do carregamento do controllerSetas: {painelObjetos.transform.childCount}");
+        Debug.Log($"quantiade de sombras no inicio do carregamento do controllerSetas: {painelLixeiras.transform.childCount}");
     }
 
     void Update()
     {
         quantItems = painelObjetos.transform.childCount;
+
+        if (travaCriacaoObjetos == false)
+        {
+            if (quantItems > 0)
+            {
+                insercaoPareamento();
+                travaCriacaoObjetos = true;
+            }
+        }
 
         if(quantItems > 0)
         {
@@ -129,8 +116,6 @@ public class ControllerSetas : MonoBehaviour
             if(substituirAposParear)
             {
                 Sprite[] arraySprites = Resources.LoadAll<Sprite>("fases/reciclagemFase1/objetoPareado"); //so alterar esse caminho para alterar a imagem pareada;
-                //Debug.Log("entrou no if de objetoPareado");
-                //Debug.Log("largura do array: "+arraySprites.Length);
                 foreach(Sprite novo in arraySprites)
                     {
                         string novoSprite = novo.name.Split('_')[1];
@@ -152,6 +137,38 @@ public class ControllerSetas : MonoBehaviour
             audioSource.PlayOneShot(somErro);
             Debug.Log("Seleção errada!");
         }
+    }
+
+    void insercaoPareamento()
+    {
+        numObjetos = painelObjetos.transform.childCount;
+        numSombras = painelLixeiras.transform.childCount;
+
+        for(int x = 0; x<numObjetos; x++)//captura os filhos do painel, pega o elemento button e adiciona a função ClicarLixo
+        {
+            GameObject filho = painelObjetos.transform.GetChild(x).gameObject;
+            GameObject filhoLocal = filho;
+            Button btn = filhoLocal.GetComponent<Button>();
+
+            btn.onClick.AddListener(() => {
+            ClicarLixo(filhoLocal);
+            });
+            Debug.Log($"Listener adicionado em {filho.name}");
+        }
+
+        for(int x = 0; x<numSombras; x++)//captura os filhos do painel, pega o elemento button e adiciona a função ClicarLixeira
+        {
+            GameObject filho = painelLixeiras.transform.GetChild(x).gameObject;
+            GameObject filhoLocal = filho;
+            Button btn = filhoLocal.GetComponent<Button>();
+
+            btn.onClick.AddListener(() => {
+            ClicarLixeira(filhoLocal);
+            });
+            Debug.Log($"Listener adicionado em {filho.name}");
+        }
+        Debug.Log("Se ele chegou ate aqui tem algo de errado, ta pulando os fors");
+        audioSource = GetComponent<AudioSource>();//saida de audio geral para o sistema de pareamento
     }
 
     void AtivarBotaoProxNivel()
